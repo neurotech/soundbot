834e7be8088a0a4111aef5d8bfd9ee4e835026e7
Tim Douglas <neurotech@gmail.com>
2017-05-24 16:11:10 +1000

    Improved communication to users

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const Discord = require('discord.js');
 const say = require('say');
 const config = require('./config');
+const details = require('./package.json');
 const library = require('./library');
 const utils = require('./utils');
 const help = require('./help');
@@ -17,10 +18,40 @@ client.on('message', message => {
   // Join/leave the user's voice channel
   if (message.content.startsWith('.join')) {
     const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel) {
+      return message.author.send({
+        embed: {
+          color: config.palette.red,
+          title: `:warning: **You are not in a voice channel!**`,
+          description: `I can't join your voice channel as you're not in one! Please join a voice channel and then make your request again.`,
+          fields: [
+            {
+              name: 'For reference, your request was:',
+              value: `\`\`\`\n${message.content}\n\`\`\``
+            }
+          ]
+        }
+      });
+    }
     voiceChannel.join();
   }
   if (message.content.startsWith('.leave')) {
     const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel) {
+      return message.author.send({
+        embed: {
+          color: config.palette.red,
+          title: `:warning: **You are not in a voice channel!**`,
+          description: `I can't leave your voice channel as you're not in one! Please join a voice channel and then make your request again.`,
+          fields: [
+            {
+              name: 'For reference, your request was:',
+              value: `\`\`\`\n${message.content}\n\`\`\``
+            }
+          ]
+        }
+      });
+    }
     voiceChannel.leave();
   }
 
@@ -50,7 +81,19 @@ client.on('message', message => {
     var chunked = message.content.split(' ');
     const voiceChannel = message.member.voiceChannel;
     if (!voiceChannel) {
-      return message.author.send(`:warning: :warning: :warning:\n\nI can't say your requested phrase as you're not in a voice channel. Please join a voice channel and then make your request again.\n\nFor reference, your request was:\n\`\`\`\n${message.content}\n\`\`\``);
+      return message.author.send({
+        embed: {
+          color: config.palette.red,
+          title: `:warning: **You are not in a voice channel!**`,
+          description: `I can't say your requested phrase as you're not in a voice channel. Please join a voice channel and then make your request again.`,
+          fields: [
+            {
+              name: 'For reference, your request was:',
+              value: `\`\`\`\n${message.content}\n\`\`\``
+            }
+          ]
+        }
+      });
     }
     voiceChannel.join()
       .then(connection => {
@@ -72,7 +115,19 @@ client.on('message', message => {
   if (message.content.startsWith('.play')) {
     const voiceChannel = message.member.voiceChannel;
     if (!voiceChannel) {
-      return message.author.send(`:warning: :warning: :warning:\n\nI can't play your requested sound as you're not in a voice channel. Please join a voice channel and then make your request again.\n\nFor reference, your request was:\n\`\`\`\n${message.content}\n\`\`\``);
+      return message.author.send({
+        embed: {
+          color: config.palette.red,
+          title: `:warning: **You are not in a voice channel!**`,
+          description: `I can't play your requested sound as you're not in a voice channel. Please join a voice channel and then make your request again.`,
+          fields: [
+            {
+              name: 'For reference, your request was:',
+              value: `\`\`\`\n${message.content}\n\`\`\``
+            }
+          ]
+        }
+      });
     }
 
     let chunked = message.content.split(' ');
@@ -97,14 +152,32 @@ client.on('message', message => {
 
   // Help messages
   if (message.content.startsWith('.list')) {
-    message.author.send(help);
+    const embed = new Discord.RichEmbed()
+      .setColor(config.palette.green)
+      .setTitle(`**Hello, I'm Soundbot** (\`Version ${details.version}\`)`)
+      .setDescription(`I will respond to the following commands:`)
+      .addField(`**:scroll: General:**`, help.general, false)
+      .addField(`**:musical_keyboard: Sounds:**`, help.sounds, false)
+      .addField(`**:speech_balloon: Speech:**`, help.speech, false);
+
+    message.author.send({embed});
   }
   if (message.content.startsWith('.sounds')) {
+    let selection = [];
+    let subSample = library.sort(() => Math.random() * 2 - 1);
+    let random = subSample.slice(0, 5);
+    for (var i = 0; i < random.length; i++) {
+      selection.push({ sound: random[i].id, description: random[i].description });
+    }
     const embed = new Discord.RichEmbed()
       .setColor(config.palette.blue)
       .setThumbnail('https://raw.githubusercontent.com/neurotech/soundbot/master/images/megaman_walk.gif')
-      .addField('Soundbot Sound Library', `[Click here to see the full list of sounds.](${config.soundlist})`, true);
-
+      .addField(`\`Soundbot Sound Library\``, `:arrow_right: [Click here to see the full list of sounds.](${config.soundlist})\n\n**Here's 5 random sounds:**`, false)
+      .addField(`\`.play ${selection[0].sound}\``, `*${selection[0].description}*\n`, false)
+      .addField(`\`.play ${selection[1].sound}\``, `*${selection[1].description}*\n`, false)
+      .addField(`\`.play ${selection[2].sound}\``, `*${selection[2].description}*\n`, false)
+      .addField(`\`.play ${selection[3].sound}\``, `*${selection[3].description}*\n`, false)
+      .addField(`\`.play ${selection[4].sound}\``, `*${selection[4].description}*`, false);
     message.author.send({embed});
   }
 });
--- help.js
+++ help.js
@@ -1,18 +1,41 @@
-module.exports =
-`:information_desk_person: I will respond to the following commands:
+let general = `
 \`\`\`
-General:
---------
-.list    List all commands
+.list
+List all commands
+\`\`\`
 
-Sounds:
--------
-.join    Join the voice channel that you're in.
-.leave   Leave the voice channel that you're in.
-.play    Play a sound file in the voice channel that you're in.
-.sounds  List all the sounds in my library.
+\`\`\`
+.join
+Join the voice channel that you're in.
+\`\`\`
 
-Speech:
--------
-.say <phrase>   Speak the <phrase> in the voice channel that you're in.
-\`\`\``;
+\`\`\`
+.leave
+Leave the voice channel that you're in.
+\`\`\`
+`;
+
+let sounds = `
+\`\`\`
+.play
+Play a sound file in the voice channel that you're in.
+\`\`\`
+
+\`\`\`
+.sounds
+List all the sounds in my library.
+\`\`\`
+`;
+
+let speech = `
+\`\`\`
+.say <phrase>
+Speak the <phrase> in the voice channel that you're in.
+\`\`\`
+`;
+
+module.exports = {
+  general: general,
+  sounds: sounds,
+  speech: speech
+};
