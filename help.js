3bc1a27c0f3de40fe1c815e7865e240565d6a64b
Tim Douglas <neurotech@gmail.com>
2017-05-26 12:04:55 +1000

    Added new commands to help

--- help.js
+++ help.js
@@ -22,6 +22,11 @@ Play a sound file in the voice channel that you're in.
 \`\`\`
 
 \`\`\`
+.randomsound
+Play a random sound file in the voice channel that you're in.
+\`\`\`
+
+\`\`\`
 .sounds
 List all the sounds in my library.
 \`\`\`
@@ -31,6 +36,21 @@ let speech = `
 \`\`\`
 .say <phrase>
 Speak the <phrase> in the voice channel that you're in.
+\`\`\`
+
+\`\`\`
+.sentence
+Generate a sentence and send it to the text channel that you made the command from.
+\`\`\`
+
+\`\`\`
+.speaksentence
+Generate a sentence and speak it into the voice channel that you're in.
+\`\`\`
+
+\`\`\`
+.ttssentence
+Generate a sentence and speak it via TTS to the text channel that you made the command from.
 \`\`\`
 `;
 
