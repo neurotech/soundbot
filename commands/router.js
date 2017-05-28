153c3df907e292111f933dd24c39f5291f3756cc
Tim Douglas <neurotech@gmail.com>
2017-05-28 12:30:32 +1000

    Convert message to lower case

--- commands/router.js
+++ commands/router.js
@@ -5,20 +5,20 @@ const sounds = require('./sounds');
 
 const router = (message) => {
   // Join/leave the user's voice channel
-  if (message.content.startsWith('.join')) channel.join(message);
-  if (message.content.startsWith('.leave')) channel.leave(message);
+  if (message.content.toLowerCase().startsWith('.join')) channel.join(message);
+  if (message.content.toLowerCase().startsWith('.leave')) channel.leave(message);
 
   // Generate semi-sensical sentences
-  if (message.content.startsWith('.sentence')) sentence.sentence(message);
-  if (message.content.startsWith('.ttssentence')) sentence.tts(message);
+  if (message.content.toLowerCase().startsWith('.sentence')) sentence.sentence(message);
+  if (message.content.toLowerCase().startsWith('.ttssentence')) sentence.tts(message);
 
   // Play a sound
-  if (message.content.startsWith('.play')) sounds.play(message);
-  if (message.content === '.randomsound') sounds.random(message);
+  if (message.content.toLowerCase().startsWith('.play')) sounds.play(message);
+  if (message.content.toLowerCase() === '.randomsound') sounds.random(message);
 
   // Help messages
-  if (message.content.startsWith('.list')) help.list(message);
-  if (message.content.startsWith('.sounds')) help.sounds(message);
+  if (message.content.toLowerCase().startsWith('.list')) help.list(message);
+  if (message.content.toLowerCase().startsWith('.sounds')) help.sounds(message);
 };
 
 module.exports = router;
