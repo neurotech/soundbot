b8159203aadf247696385ad167f19a26b7ef639c
Tim Douglas <tim.douglas@phocassoftware.com>
2018-05-09 23:30:33 +1000

    Improved base app.js

--- app.js
+++ app.js
@@ -1,38 +1,52 @@
-const Discord = require('discord.js');
-const symbols = require('log-symbols');
-const dateFormat = require('dateformat');
-const config = require('./config');
-const router = require('./commands/router');
-const client = new Discord.Client();
+const client = require("./discord-client");
+const log = require("./log");
+const config = require("./config");
+const web = require("./web");
+const router = require("./commands/router");
 
-client.login(config.discord.token);
+web.start();
 
 // When ready, set currently playing game to help message
-client.on('ready', () => {
-  client.user.setGame('Type .list for help');
-  console.log(symbols.success, ` [${dateFormat()}] Soundbot online.`);
+client.on("ready", () => {
+  client.user
+    .setActivity("for a command from .list", {
+      type: "WATCHING"
+    })
+    .then(presence => log("success", "Soundbot online."))
+    .catch(err => log("error", error.message));
 });
 
-// Watch for message
-client.on('message', message => {
+// Watch for messages
+client.on("message", message => {
   // Ignore DMs and only respond to messages from text channels.
-  if (message.channel.type !== 'text') { return; }
+  if (message.channel.type !== "text") return;
 
-  // Send message to router
+  // Send the message to router
   router(message);
 });
 
-// Re-login if client goes offline
+// Try to re-login if client goes offline
 setInterval(() => {
   if (client.status === 1) {
-    console.log(symbols.error, ` [${dateFormat()}] Soundbot lost connection to Discord!`);
-    client.login(config.discord.token)
-      .then(() => {
-        console.log(symbols.success, ` [${dateFormat()}] Soundbot re-connected to Discord.`);
-      });
+    log("warning", "Soundbot lost connection to Discord!");
+    client.login(config.discord.token).then(() => {
+      log("success", "Soundbot re-connected to Discord.");
+    });
   }
 }, 1000 * 30);
 
-// Log out on process exit/uncaught exception
-process.on('exit', () => { client.destroy(); });
-process.on('uncaughtException', () => { client.destroy(); });
+// Log out on process exit/uncaught exception/unhandled rejection
+process.on("exit", () => {
+  client.destroy();
+  log("error", "Shutting down Soundbot.");
+});
+process.on("uncaughtException", err => {
+  client.destroy();
+  log("error", `Uncaught exception!`);
+  log("error", err);
+  process.exit(1);
+});
+process.on("unhandledRejection", (reason, p) => {
+  client.destroy();
+  log("error", `Unhandled Rejection: ${reason.stack}`);
+});
