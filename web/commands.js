37bcdbec8ee70748ff347a47ca93f7a63972c323
Tim Douglas <tim.douglas@phocassoftware.com>
2018-05-15 00:38:10 +1000

    Added playsound endpoint

--- web/commands.js
+++ web/commands.js
@@ -13,9 +13,48 @@ let commands = {
       var result = `DND Mode is now ${tokens.onOff.toUpperCase()}.`;
       log("info", result);
       util.sendResponse(200, "OK", result, response);
+    } else if (tokens.onOff === "") {
+      let dnd = db.get("state.dnd").value();
+      let dndState = dnd === 1 ? "on" : "off";
+      util.sendResponse(200, "OK", dndState, response);
     }
   },
   sound: {
+    play: function(request, response, tokens) {
+      let dnd = db.get("state.dnd").value();
+      let payload = "";
+      if (dnd === 1) {
+        util.sendResponse(
+          503,
+          "UNAVAILABLE",
+          "Do not disturb is on.",
+          response
+        );
+      } else {
+        request.on("data", function(data) {
+          payload += data;
+        });
+        request.on("end", function() {
+          let soundObject = JSON.parse(payload);
+          if (tokens.channelId && typeof tokens.channelId === "string") {
+            var channelId = tokens.channelId.toString();
+            var isValidChannelId = db
+              .get("discord.channels.voice")
+              .find({ id: channelId })
+              .value();
+
+            if (isValidChannelId) {
+              sounds.play(null, channelId, soundObject, (err, result) => {
+                if (err) util.sendResponse(400, "ERROR", err, response);
+                util.sendResponse(200, "OK", result, response);
+              });
+            } else {
+              util.sendResponse(400, "ERROR", "Invalid channel ID!", response);
+            }
+          }
+        });
+      }
+    },
     random: function(request, response, tokens) {
       var dnd = db.get("state.dnd").value();
       if (dnd === 1) {
@@ -32,8 +71,10 @@ let commands = {
             .get("discord.channels.voice")
             .find({ id: channelId })
             .value();
+
           if (isValidChannelId) {
             sounds.random(null, channelId, (err, result) => {
+              if (err) util.sendResponse(400, "ERROR", err, response);
               util.sendResponse(200, "OK", result, response);
             });
           } else {
