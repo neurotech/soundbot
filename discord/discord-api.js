d2e79c1a632ffa5c1888316fe8011aa0c4e3d8f6
Tim Douglas <tim.douglas@phocassoftware.com>
2018-05-14 20:05:29 +1000

    Modularised Discord

---
+++ discord/discord-api.js
@@ -0,0 +1,68 @@
+const tiny = require("tiny-json-http");
+const spacetime = require("spacetime");
+const discord = require("./discord-client");
+const config = require("../config");
+const db = require("../db");
+
+module.exports = {
+  getChannels: callback => {
+    var url = `https://discordapp.com/api/guilds/${
+      config.discord.guildId
+    }/channels`;
+
+    tiny.get(
+      {
+        url: url,
+        headers: { Authorization: `Bot ${config.discord.token}` }
+      },
+      (err, result) => {
+        if (err) {
+          return callback(err);
+        } else {
+          var json = processChannelList(result.body);
+          callback(null, json);
+        }
+      }
+    );
+  }
+};
+
+let processChannelList = json => {
+  var now = spacetime.now();
+  var channels = {
+    freshness: now.goto("Australia/Sydney").format("nice-day"),
+    text: [],
+    voice: []
+  };
+
+  json.forEach(channel => {
+    if (
+      channel.type === 0 &&
+      config.discord.blacklist.text.indexOf(channel.name) === -1
+    )
+      channels.text.push({
+        id: channel.id,
+        name: channel.name,
+        position: channel.position
+      });
+
+    if (
+      channel.type === 2 &&
+      config.discord.blacklist.voice.indexOf(channel.name) === -1
+    )
+      channels.voice.push({
+        id: channel.id,
+        name: channel.name,
+        position: channel.position
+      });
+  });
+
+  channels.text.sort((a, b) => {
+    return a.position > b.position;
+  });
+  channels.voice.sort((a, b) => {
+    return a.position > b.position;
+  });
+
+  return channels;
+};
