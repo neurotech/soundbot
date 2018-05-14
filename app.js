a1d4ef5b468a23751f78de2d203485bc0190ef68
Tim Douglas <tim.douglas@phocassoftware.com>
2018-05-14 20:07:15 +1000

    App now runs setup on start

--- app.js
+++ app.js
@@ -1,39 +1,67 @@
-const client = require("./discord-client");
 const log = require("./log");
 const config = require("./config");
+const db = require("./db");
+const setup = require("./setup");
 const web = require("./web");
+const tasks = require("./tasks");
+const client = require("./discord/discord-client");
 const router = require("./commands/router");
 
-web.start();
+db.defaults(config.db.defaults).write();
+setup((err, results) => {
+  if (err) {
+    log("error", err);
+  } else {
+    log("info", `DND Mode is ${db.get("state.dnd") === 0 ? "off" : "on"}.`);
+    log(
+      "success",
+      `DB seeded with ${results.text.length} text and ${
+        results.voice.length
+      } voice channels.`
+    );
 
-// When ready, set currently playing game to help message
-client.on("ready", () => {
-  client.user
-    .setActivity("for a command from .list", {
-      type: "WATCHING"
-    })
-    .then(presence => log("success", "Soundbot online."))
-    .catch(err => log("error", error.message));
-});
+    // Start the web server
+    web.start();
 
-// Watch for messages
-client.on("message", message => {
-  // Ignore DMs and only respond to messages from text channels.
-  if (message.channel.type !== "text") return;
-
-  // Send the message to router
-  router(message);
-});
-
-// Try to re-login if client goes offline
-setInterval(() => {
-  if (client.status === 1) {
-    log("warning", "Soundbot lost connection to Discord!");
-    client.login(config.discord.token).then(() => {
-      log("success", "Soundbot re-connected to Discord.");
+    // Schedule recurring tasks
+    tasks.schedule((err, tasks) => {
+      if (err) {
+        log("error", err);
+      } else {
+        log("success", `Scheduled ${tasks} task(s).`);
+      }
     });
+
+    // When ready, set currently playing game to help message
+    client.on("ready", () => {
+      client.user
+        .setActivity("for a command from .list", {
+          type: "WATCHING"
+        })
+        .then(presence => log("success", "Soundbot online."))
+        .catch(err => log("error", error.message));
+    });
+
+    // Watch for messages
+    client.on("message", message => {
+      // Ignore DMs and only respond to messages from text channels.
+      if (message.channel.type !== "text") return;
+
+      // Send the message to router
+      router(message);
+    });
+
+    // Try to re-login if client goes offline
+    setInterval(() => {
+      if (client.status === 1) {
+        log("warning", "Soundbot lost connection to Discord!");
+        client.login(config.discord.token).then(() => {
+          log("success", "Soundbot re-connected to Discord.");
+        });
+      }
+    }, 1000 * 30);
   }
-}, 1000 * 30);
+});
 
 // Log out on process exit/uncaught exception/unhandled rejection
 process.on("exit", () => {
