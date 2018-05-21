dc0fa812ac64a328fe8d4746f33364bf565ce5b9
Tim Douglas <tim.douglas@phocassoftware.com>
2018-05-22 00:04:46 +1000

    Validate api and commands' auth header

--- web/api.js
+++ web/api.js
@@ -1,16 +1,56 @@
+const fs = require("fs");
 const tiny = require("tiny-json-http");
 const config = require("../config");
 const db = require("../db");
+const validate = require("./auth/validate");
 const util = require("./util");
 
 let api = {
   web: {
     discord: {
       channels: function(request, response, tokens) {
-        var channels = db.get("discord.channels");
-        response.writeHead(200, { "Content-Type": "application/json" });
-        response.write(JSON.stringify(channels));
-        response.end();
+        let valid = false;
+        if (request.headers.authorization) {
+          valid = validate(request.headers.authorization);
+        }
+
+        if (valid) {
+          var channels = db.get("discord.channels");
+          response.writeHead(200, { "Content-Type": "application/json" });
+          response.write(JSON.stringify(channels));
+          response.end();
+        } else {
+          util.sendResponse(
+            401,
+            "Unauthorized",
+            "You are not authorized to perform this action.",
+            response
+          );
+        }
+      }
+    },
+    sounds: {
+      list: function(request, response, tokens) {
+        let valid = false;
+        if (request.headers.authorization) {
+          valid = validate(request.headers.authorization);
+        }
+
+        if (valid) {
+          let sounds = require("../sounds.json");
+          response.writeHead(200, {
+            "Content-Type": "application/json"
+          });
+          response.write(JSON.stringify(sounds));
+          response.end();
+        } else {
+          util.sendResponse(
+            401,
+            "Unauthorized",
+            "You are not authorized to perform this action.",
+            response
+          );
+        }
       }
     }
   }
--- web/commands.js
+++ web/commands.js
@@ -2,180 +2,332 @@ const crypto = require("crypto");
 const sounds = require("../commands/sounds");
 const sentence = require("../commands/sentence");
 const db = require("../db");
+const validate = require("./auth/validate");
 const util = require("./util");
 const log = require("../log");
 const queue = require("../action-queue");
 
 let commands = {
   dnd: (request, response, tokens) => {
-    if (tokens.onOff && typeof tokens.onOff === "string") {
-      var state = 0;
-      if (tokens.onOff === "on") state = 1;
-      if (tokens.onOff === "off") state = 0;
-      db.set("state.dnd", state).write();
-      var result = `DND Mode is now ${tokens.onOff.toUpperCase()}.`;
-      log("info", result);
-      util.sendResponse(200, "OK", result, response);
-    } else if (tokens.onOff === "") {
-      let dnd = db.get("state.dnd").value();
-      let dndState = dnd === 1 ? "on" : "off";
-      util.sendResponse(200, "OK", dndState, response);
+    let valid = false;
+    if (request.headers.authorization) {
+      valid = validate(request.headers.authorization);
+    }
+
+    if (valid) {
+      if (tokens.onOff) {
+        var state = 0;
+        if (tokens.onOff === "on") state = 1;
+        if (tokens.onOff === "off") state = 0;
+        db.set("state.dnd", state).write();
+        let result = `DND Mode is now ${tokens.onOff.toUpperCase()}.`;
+        log("info", result);
+        util.sendResponse(200, "OK", result, response);
+      } else if (tokens.onOff === "") {
+        let dnd = db.get("state.dnd").value();
+        let dndState = dnd === 1 ? "on" : "off";
+        util.sendResponse(200, "OK", dndState, response);
+      }
+    } else {
+      util.sendResponse(
+        401,
+        "Unauthorized",
+        "You are not authorized to perform this action.",
+        response
+      );
     }
   },
   sound: {
     play: function(request, response, tokens) {
-      let dnd = db.get("state.dnd").value();
-      let payload = "";
-      if (dnd === 1) {
+      let valid = false;
+      if (request.headers.authorization) {
+        valid = validate(request.headers.authorization);
+      }
+
+      if (valid) {
+        let dnd = db.get("state.dnd").value();
+        let payload = "";
+        if (dnd === 1) {
+          util.sendResponse(
+            503,
+            "Unavailable",
+            "Do not disturb is on.",
+            response
+          );
+        } else {
+          request.on("data", function(data) {
+            payload += data;
+          });
+          request.on("end", function() {
+            let soundObject = JSON.parse(payload);
+            if (tokens.channelId) {
+              var channelId = tokens.channelId.toString();
+              var isValidChannelId = db
+                .get("discord.channels.voice")
+                .find({ id: channelId })
+                .value();
+
+              if (isValidChannelId) {
+                let queueId = crypto.randomBytes(16).toString("hex");
+                queue.add(
+                  function(cb) {
+                    sounds.play(
+                      null,
+                      channelId,
+                      soundObject,
+                      queueId,
+                      (err, result) => {
+                        if (err) util.sendResponse(400, "ERROR", err, response);
+                        util.sendResponse(200, "OK", result, response);
+                        cb(null, result);
+                      }
+                    );
+                  },
+                  {
+                    queueId: queueId,
+                    username: soundObject.author,
+                    action: "Sound",
+                    tag: "tag-pink",
+                    destination: "📢 " + isValidChannelId.name
+                  }
+                );
+              } else {
+                util.sendResponse(
+                  400,
+                  "ERROR",
+                  "Invalid channel ID!",
+                  response
+                );
+              }
+            }
+          });
+        }
+      } else {
         util.sendResponse(
-          503,
-          "UNAVAILABLE",
-          "Do not disturb is on.",
+          401,
+          "Unauthorized",
+          "You are not authorized to perform this action.",
           response
         );
-      } else {
-        request.on("data", function(data) {
-          payload += data;
-        });
-        request.on("end", function() {
-          let soundObject = JSON.parse(payload);
-          if (tokens.channelId) {
-            var channelId = tokens.channelId.toString();
-            var isValidChannelId = db
-              .get("discord.channels.voice")
-              .find({ id: channelId })
-              .value();
-
-            if (isValidChannelId) {
-              sounds.play(null, channelId, soundObject, (err, result) => {
-                if (err) util.sendResponse(400, "ERROR", err, response);
-                util.sendResponse(200, "OK", result, response);
-              });
-            } else {
-              util.sendResponse(400, "ERROR", "Invalid channel ID!", response);
-            }
-          }
-        });
       }
     },
     random: function(request, response, tokens) {
-      var dnd = db.get("state.dnd").value();
-      if (dnd === 1) {
+      let valid = false;
+      if (request.headers.authorization) {
+        valid = validate(request.headers.authorization);
+      }
+
+      if (valid) {
+        let dnd = db.get("state.dnd").value();
+        let payload = "";
+        if (dnd === 1) {
+          util.sendResponse(
+            503,
+            "Unavailable",
+            "Do not disturb is on.",
+            response
+          );
+        } else {
+          request.on("data", function(data) {
+            payload += data;
+          });
+          request.on("end", function() {
+            let author = JSON.parse(payload);
+            if (tokens.channelId) {
+              var channelId = tokens.channelId.toString();
+              var isValidChannelId = db
+                .get("discord.channels.voice")
+                .find({ id: channelId })
+                .value();
+
+              if (isValidChannelId) {
+                let queueId = crypto.randomBytes(16).toString("hex");
+                queue.add(
+                  function(cb) {
+                    sounds.random(null, channelId, queueId, (err, result) => {
+                      if (err) util.sendResponse(400, "ERROR", err, response);
+                      util.sendResponse(200, "OK", result, response);
+                      cb(null, result);
+                    });
+                  },
+                  {
+                    queueId: queueId,
+                    username: author,
+                    action: "Random Sound",
+                    tag: "tag-indigo",
+                    destination: "📢 " + isValidChannelId.name
+                  }
+                );
+              } else {
+                util.sendResponse(
+                  400,
+                  "ERROR",
+                  "Invalid channel ID!",
+                  response
+                );
+              }
+            }
+          });
+        }
+      } else {
         util.sendResponse(
-          503,
-          "UNAVAILABLE",
-          "Do not disturb is on.",
+          401,
+          "Unauthorized",
+          "You are not authorized to perform this action.",
           response
         );
-      } else {
-        if (tokens.channelId) {
-          var channelId = tokens.channelId.toString();
-          var isValidChannelId = db
-            .get("discord.channels.voice")
-            .find({ id: channelId })
-            .value();
-
-          if (isValidChannelId) {
-            sounds.random(null, channelId, (err, result) => {
-              if (err) util.sendResponse(400, "ERROR", err, response);
-              util.sendResponse(200, "OK", result, response);
-            });
-          } else {
-            util.sendResponse(400, "ERROR", "Invalid channel ID!", response);
-          }
-        }
       }
     }
   },
   text: {
     sentence: function(request, response, tokens) {
-      var dnd = db.get("state.dnd").value();
-      if (dnd === 1) {
+      let valid = false;
+      if (request.headers.authorization) {
+        valid = validate(request.headers.authorization);
+      }
+
+      if (valid) {
+        var dnd = db.get("state.dnd").value();
+        if (dnd === 1) {
+          util.sendResponse(
+            503,
+            "Unavailable",
+            "Do not disturb is on.",
+            response
+          );
+        } else {
+          let payload = "";
+          request.on("data", function(data) {
+            payload += data;
+          });
+          request.on("end", function() {
+            let author = payload;
+            if (tokens.channelId) {
+              var channelId = tokens.channelId.toString();
+              var isValidChannelId = db
+                .get("discord.channels.text")
+                .find({ id: channelId })
+                .value();
+
+              if (isValidChannelId) {
+                let queueId = crypto.randomBytes(16).toString("hex");
+                queue.add(
+                  function(cb) {
+                    sentence.sentence(
+                      null,
+                      channelId,
+                      author,
+                      queueId,
+                      (err, result) => {
+                        if (err) {
+                          util.sendResponse(400, "ERROR", err, response);
+                        }
+                        util.sendResponse(200, "OK", result, response);
+                        cb(null, result);
+                      }
+                    );
+                  },
+                  {
+                    queueId: queueId,
+                    username: author,
+                    action: "Text Sentence",
+                    tag: "tag-azure",
+                    destination: "🔵 " + isValidChannelId.name
+                  }
+                );
+              } else {
+                util.sendResponse(
+                  400,
+                  "ERROR",
+                  "Invalid channel ID!",
+                  response
+                );
+              }
+            }
+          });
+        }
+      } else {
         util.sendResponse(
-          503,
-          "UNAVAILABLE",
-          "Do not disturb is on.",
+          401,
+          "Unauthorized",
+          "You are not authorized to perform this action.",
           response
         );
-      } else {
-        let payload = "";
-        request.on("data", function(data) {
-          payload += data;
-        });
-        request.on("end", function() {
-          let author = payload;
-          if (tokens.channelId) {
-            var channelId = tokens.channelId.toString();
-            var isValidChannelId = db
-              .get("discord.channels.text")
-              .find({ id: channelId })
-              .value();
-
-            if (isValidChannelId) {
-              let queueId = crypto.randomBytes(16).toString("hex");
-              queue.add(
-                function(cb) {
-                  sentence.sentence(
-                    null,
-                    channelId,
-                    author,
-                    queueId,
-                    (err, result) => {
-                      if (err) {
-                        util.sendResponse(400, "ERROR", err, response);
-                      }
-                      util.sendResponse(200, "OK", result, response);
-                      cb(null, result);
-                    }
-                  );
-                },
-                {
-                  queueId: queueId,
-                  username: author,
-                  action: "Text Sentence",
-                  destination: isValidChannelId.name
-                }
-              );
-            } else {
-              util.sendResponse(400, "ERROR", "Invalid channel ID!", response);
-            }
-          }
-        });
       }
     },
     ttsSentence: function(request, response, tokens) {
-      var dnd = db.get("state.dnd").value();
-      if (dnd === 1) {
+      let valid = false;
+      if (request.headers.authorization) {
+        valid = validate(request.headers.authorization);
+      }
+
+      if (valid) {
+        var dnd = db.get("state.dnd").value();
+        if (dnd === 1) {
+          util.sendResponse(
+            503,
+            "Unavailable",
+            "Do not disturb is on.",
+            response
+          );
+        } else {
+          let payload = "";
+          request.on("data", function(data) {
+            payload += data;
+          });
+          request.on("end", function() {
+            let author = payload;
+            if (tokens.channelId) {
+              var channelId = tokens.channelId.toString();
+              var isValidChannelId = db
+                .get("discord.channels.text")
+                .find({ id: channelId })
+                .value();
+
+              if (isValidChannelId) {
+                let queueId = crypto.randomBytes(16).toString("hex");
+                queue.add(
+                  function(cb) {
+                    sentence.tts(
+                      null,
+                      channelId,
+                      author,
+                      queueId,
+                      (err, result) => {
+                        if (err) {
+                          util.sendResponse(400, "ERROR", err, response);
+                        }
+                        util.sendResponse(200, "OK", result, response);
+                        cb(null, result);
+                      }
+                    );
+                  },
+                  {
+                    queueId: queueId,
+                    username: author,
+                    action: "TTS Sentence",
+                    tag: "tag-green",
+                    destination: "🔵 " + isValidChannelId.name
+                  }
+                );
+              } else {
+                util.sendResponse(
+                  400,
+                  "ERROR",
+                  "Invalid channel ID!",
+                  response
+                );
+              }
+            }
+          });
+        }
+      } else {
         util.sendResponse(
-          503,
-          "UNAVAILABLE",
-          "Do not disturb is on.",
+          401,
+          "Unauthorized",
+          "You are not authorized to perform this action.",
           response
         );
-      } else {
-        let payload = "";
-        request.on("data", function(data) {
-          payload += data;
-        });
-        request.on("end", function() {
-          let author = payload;
-          if (tokens.channelId) {
-            var channelId = tokens.channelId.toString();
-            var isValidChannelId = db
-              .get("discord.channels.text")
-              .find({ id: channelId })
-              .value();
-
-            if (isValidChannelId) {
-              sentence.tts(null, channelId, author, (err, result) => {
-                if (err) util.sendResponse(400, "ERROR", err, response);
-                util.sendResponse(200, "OK", result, response);
-              });
-            } else {
-              util.sendResponse(400, "ERROR", "Invalid channel ID!", response);
-            }
-          }
-        });
       }
     }
   }
