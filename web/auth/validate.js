7a052da1d84fbaf97c02b6d7a2ee9fd9dce9aa81
Tim Douglas <tim.douglas@phocassoftware.com>
2018-09-25 21:21:12 +1000

    Switched to a simpler auth approach

--- web/auth/validate.js
+++ web/auth/validate.js
@@ -1,45 +1,35 @@
-const AWS = require("aws-sdk");
-const rollbar = require("rollbar");
+const tiny = require("tiny-json-http");
 const config = require("../../config");
-const db = require("../../db");
 const log = require("../../log");
-
-AWS.config.update({
-  credentials: {
-    accessKeyId: config.aws.accessKeyId,
-    secretAccessKey: config.aws.secretAccessKey
-  },
-  region: "us-west-1"
-});
-
-let dynamo = new AWS.DynamoDB.DocumentClient();
 
 module.exports = (authorization, callback) => {
   if (authorization !== null || authorization !== undefined) {
     let chunked = authorization.split("|");
-    let username = chunked[0];
     let token = chunked[1];
 
-    let validationQuery = {
-      TableName: "Soundbot",
-      ExpressionAttributeValues: {
-        ":t": token
-      },
-      KeyConditionExpression: "discordToken = :t"
-    };
-
-    dynamo.query(validationQuery, function(err, data) {
-      if (err || data.Items.length === 0) return callback(err);
-
-      if (data.Items.length === 1) {
-        callback(null, true);
-      } else {
-        log(
-          "warning",
-          `New authentication attempt! User: ${username} Token: ${token}`
-        );
-        callback(null, false);
-      }
-    });
+    if (token !== null || token !== undefined) {
+      let url = "https://discordapp.com/api/users/@me";
+      tiny.get(
+        {
+          url: url,
+          headers: { Authorization: `Bearer ${token}` }
+        },
+        (err, result) => {
+          if (err) return callback(err);
+          let whitelist = config.access.split("|");
+          let username = result.body.username;
+          let verified = whitelist.indexOf(username) > -1;
+          if (verified) {
+            callback(null, true);
+          } else {
+            log(
+              "warning",
+              `New authentication attempt! User: ${username} Token: ${token}`
+            );
+            callback(null, false);
+          }
+        }
+      );
+    }
   }
 };
